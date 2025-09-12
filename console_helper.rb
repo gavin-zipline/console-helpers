121d7f627d45430a65375bdfd33c14a9fcb81ea7
gavin-zipline <86084376+gavin-zipline@users.noreply.github.com>
2025-09-11 18:40:07 -0700

    update

--- console_helper.rb
+++ console_helper.rb
@@ -1,4 +1,4 @@
-CONSOLE_HELPER_VERSION = "0.3.24"
+CONSOLE_HELPER_VERSION = "0.3.25"
 # == LOADED HELPERS REGISTRY ==
 # Tracks loaded helpers, their versions, and cheatsheet procs
 unless defined?(ConsoleHelpers)
